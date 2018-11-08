09fa3a888b0e38cb09865e67644ed9463479c837
Kusakin Denis <kusakindenis@gmail.com>
2018-11-08 19:17:49 +0300

    second commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,13 @@ repositories {
     jcenter()
 }
 
+val jcrVersion = "2.4.0"
+
 dependencies {
     implementation(kotlin("stdlib", "1.2.31"))
+    implementation("org.apache.jackrabbit:jackrabbit-jcr-client:$jcrVersion")
+    implementation("org.apache.jackrabbit:jackrabbit-jcr2dav:$jcrVersion")
+    implementation("org.apache.jackrabbit:jackrabbit-spi2dav:$jcrVersion")
+    implementation("org.apache.jackrabbit:jackrabbit-jcr2spi:$jcrVersion")
+    implementation("javax.jcr:jcr:2.0")
 }
---
+++ src/main/kotlin/org/deniskusakin/aemlibrary/App.kt
@@ -0,0 +1,8 @@
+package org.deniskusakin.aemlibrary
+
+/**
+ * @author Denis_Kusakin. 11/8/2018.
+ */
+//fun main(args: Array<String>) {
+//    main()
+//}
